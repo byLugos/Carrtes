e1130d5fee2c41742ad594370b661d9aab60278f
Diego-Figue <diegofigue0212@gmail.com>
2024-04-25 15:22:42 -0500

    Firts global component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-full bg-slate-50">
       <h1> TEST </h1>
     </main>
   );
---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[80px] bg-slate-600 fixed">
+      <h1>esto es un NavBar</h1>
+    </nav>
+  );
+};
+
+export { Navbar };
