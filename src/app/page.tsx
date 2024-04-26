5afe2f3682c68923fc707c02bd63033015032107
Diego-Figue <diegofigue0212@gmail.com>
2024-04-26 14:10:25 -0500

    Fix styles and create logic for the sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,22 @@ import { ButtonTest } from "./ui/Global/buttonTest";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-yellow-50 flex justify-start items-center flex-col">
+    <main className="w-full h-screen bg-whiteBg flex justify-start items-center flex-col">
       <div className="w-full flex flex-col mt-7 gap-6 text-center">
-      <h1 className="text-rose-400 font-bold text-2xl">Carrtes<span className="text-slate-950">  ¡con un toque de amor y creatividad!  </span></h1>
-      <p className="text-slate-950"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum. </p>
+        <h1 className="text-hardRose font-bold text-2xl">
+          Carrtes
+          <span className="text-textTitle">
+            {" "}
+            ¡con un toque de amor y creatividad!{" "}
+          </span>
+        </h1>
+        <p className="text-textParagraph">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl
+          nunc mi ipsum.{" "}
+        </p>
       </div>
     </main>
   );
- }
+}
--- src/app/ui/Global/navbar.tsx
+++ src/app/ui/Global/navbar.tsx
@@ -1,9 +1,45 @@
 "use client";
 
+//imports icons
+import { Bars3Icon } from "@heroicons/react/16/solid";
+
+import { useState } from "react";
+
+import "./transitionNavbar.css";
+
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleMenu = () => {
+    return setToggle(!toggle);
+  };
+
   return (
-    <nav className="w-full h-[80px] bg-slate-600 fixed top-0">
-      <h1>esto es un NavBar</h1>
+    <nav className="w-full h-[70px] bg-whiteBg fixed top-0 ">
+      <div className="px-5 h-full flex items-center justify-between">
+        <figure>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjf61OnnHinB_OYbQ05C8bNLRpx6_9GCJjnKdn6JDBUQ&s"
+            alt="Logo Carrtes"
+            className="w-[44px]"
+          />
+        </figure>
+
+        <span
+          className=" w-[59px] h-[59px] flex justify-center border border-cyan-100 rounded-[100%] cursor-pointer"
+          onClick={toggleMenu}
+        >
+          <Bars3Icon className="w-[24px] text-hardRose" />
+        </span>
+      </div>
+
+      <div
+        className={`w-[100%] h-screen absolute top-[70px] bg-[rgba(0,0,0,50%)] flex justify-end ${
+          toggle ? "vissible" : "hidde"
+        }`}
+      >
+        <div className="w-[300px] h-full bg-white"></div>
+      </div>
     </nav>
   );
 };
